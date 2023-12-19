c4d8b1222767667ca8bef306d97e4b26ff76d8e8
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-19 20:48:49 +0200

    Concatenation

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ let box2; // undefined
 
 box2 = 15; // 15
 
-const secretWord = 'superSecret!Kyivstar!Cybersecuruti';
-console.log(secretWord);
+let secretWord = 15;
+//console.log(secretWord);
 
-// secretWord = 'worddd'; <<<--- перевизначити константу неможливо
+secretWord = 'worddd';
+
+//console.log(box2); // Не виконається через помилку на рядку 10
+// ....
+
+let result1 = 1 + 2; // 3
+//console.log(result1);
+let result2 = 3 - 2; // 1
+//console.log(result2);
+let result3 = 2 * 2; // 4
+//console.log(result3);
+let result4 = 4 / 2; // 2
+//console.log(result4);
+
+let result5 = 0.1 + 0.2; // 0.3
+//console.log(result5);
+
+let notAllowed = 10 / 0;
+// console.log(notAllowed); // Infinity (Нескінченність)
+
+let brrrr = 10 / 'hello world';
+//console.log(brrrr); // NaN
+// NaN-Not a Number -->> операція, неприпустима з точки зору математики
+
+// "+"
+// складає числа
+// Зклеювати строки (конкатенація)
+let newLine = 'Hello' + 'world';
+//console.log(newLine);
+
+newLine = 5 + 'hello';
+//console.log(newLine);
+
+newLine = '1' + 1;
+console.log(newLine); // 11
