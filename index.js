e333b6df86b3e35da3ea5068c799bb503116eceb
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-26 19:07:23 +0200

    Scope welcome

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
-//const value1 = Number(prompt('Введіть перше число: '));
-//const value2 = Number(prompt('Введіть друге число: '));
+const num1 = Number(prompt('Введіть перше число: '));
+const num2 = Number(prompt('Введіть друге число: '));
 
-// Задача 1. Написати функцію, яка прицмає два числа і повертає те число, що більше
+// Задача: Написати функцію, яка приймає 2 числа і додає їх
 
-
-function getBigger(number1, number2) { // number1, number2 - формальні параметри
-    if(typeof number1 !== 'number' || typeof number2 !== 'number') {
-        console.log('Ми очікуємо числа!');
-    } else if(number1 > number2) {
-        return number1;
-    } else {
-        return number2;
-    }
+function addTwoNumbers(arg1, arg2) {
+    console.log(num2);
+    return arg1 + arg2;
 }
 
-console.log(getBigger(value1, value2)); // value1, value2 - фактичні параметри
+const result = addTwoNumbers(num1, num2);
+console.log(result);
 
+// Область видимості - визначення доступності і видимості змінних та функцій у певних частинах коду
 
+/* 
+У JS існує 2 основні області видимості:
 
-// Задача 2. Написати функцію, яка обчислює середнє арифметичне з двох чисел
-
-/*
-function average(number1, number2) {
-    return (number1 + number2) / 2;
-}
-
-console.log(average(value1, value2));
+1. Глобальна область видимості - глобальна область видимості охоплює весь документ (программу)
 */
 
-// Задача 3. Написати функцію, яка приймає число і визначає, чи є воно парним
+const globalVariable = 10;
 
-/*
-function isEven(number) {
-    if(number % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+function globalFunction() {
+    // тіло функції...
 }
 
-console.log(isEven(5));
+console.log(globalVariable); // 10
+
+// 2. Локальна область видимості - визначається блоками коду (фігурними дужками)
+
+function testFunction() {
+    const localVariable = 20; // Локальна змінна testFunction
+
+    if(true) {
+        const innerVariable = 30; // Локальна змінна if
+        console.log(innerVariable); // 30
+        console.log(localVariable); // 20 , localVariable - знаходиться у глобальній області видимості по відношенню до if
+        console.log(globalVariable); // 10 , globalVariable - знаходиться у глобальній області видимості по відношенню до if
+    }
+
+    console.log(localVariable); // 20
+    // console.log(innerVariable); => Помилка: innerVariable недоступна тут
+}
+
+testFunction();
+
+// console.log(localVariable); => Помилка: localVariable недоступна тут
+
+// 3. Функціональна область видимості - визначає область видимості, за якої всі змінні доступні всередині функцій, незалежно від блоків, яких вони оголошені
+// (var)
+
+
+// Summary
+/*
+Scope - область видимості
+(це всі наші змінні, об'єкти ітд, які нам доступні)
+
+Local Scope (локальна область видимості) - це всі наші змінні, об'єкти ітд ВСЕРЕДИНІ нашої функції
+
+Global Scope (глобальна область видимості) - це всі наші змінні, об'єкти ітд з поточного коду
 
 */
