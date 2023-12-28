c2114dbce74dc6afe5b774f725a4a4e16508400e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-28 18:56:25 +0200

    Links

--- index.js
+++ index.js
@@ -1,66 +1,9 @@
-/* Задача 2 (за бажанням)
-
-Зробити калькулятор =)
-
-Користувач послідовно (один за одним) вводить
-- Перше число
-- Друге число
-- Один з варіантів: +, -, *, /
-
-Для кожної математичної дії (+, -, *, /) зробіть окрему функцію.
-Весь калькулятор - це теж окрема функція, роль якої - спитати у користувача два числа і дію і повернути йому результат в якості alert.
-
-P.S. Повна творча свобода.
-Якщо ви бачите рішення цієї задачі якось по-іншому - Ви можете реалізувати своє бачення калькулятора.
-
-*/
-
-const operand1 = Number(prompt('Введіть перше число'));
-const operand2 = Number(prompt('Введіть друге число'));
-const action = prompt('Введіть потрібну дію: +, -, *, /');
-
-function calculator(operand1, operand2, action) {
-    let result; // тут буде лежати остаточний результат, після дій над операндами
-
-    switch(action) {
-        case '+': {
-            result = sum(operand1, operand2);
-            break;
-        }
-        case '-': {
-            result = substract(operand1, operand2);
-            break;
-        }
-        case '*': {
-            result = multy(operand1, operand2);
-            break;
-        }
-        case '/': {
-            result = divide(operand1, operand2);
-            break;
-        }
-        default: {
-            result = 'Сталася помилка! Введіть правильну дію +, -, *, /';
-        }
-    }
-
-    console.log(result);
-}
-
-calculator(operand1, operand2, action);
-
 function sum(a, b) {
     return a + b;
 }
 
-function substract(a, b) {
-    return a - b;
-}
+let superFunction = sum; // насправді, sum - це посилання на комірку в RAM
 
-function multy(a, b) {
-    return a * b;
-}
+console.log(sum(2, 4)); // 0X000045
 
-function divide(a, b) {
-    return a / b;
-}
+console.log(superFunction(5, 5)); // 0X000045
