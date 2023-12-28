e985f892b284da0862c2dc8f7e3c4257b4454e4c
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-28 18:34:59 +0200

    Calculator task resolve

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-/* Задача 1 :^
-
-prompt
-Виведіть користувачу пропозицію меню:
-1 - Сік
-2 - Вода
-3 - Кава
-4 - Чай
-5 - Лимонад
-
-Вивести в консоль напій, цифру якого ввів користувач
-Зробіть, будь ласка, через switch-case
-
-*/
-
-const userChoice = Number(prompt('Оберіть напій: \n1 - Сік \n2 - Вода \n3 - Кава \n4 - Чай \n5 - Лимонад'));
-
-switch(userChoice) {
-    case 1: {
-        console.log('Сік');
-        break;
-    }
-    case 2: {
-        console.log('Вода');
-        break;
-    }
-    case 3: {
-        console.log('Кава');
-        break;
-    }
-    case 4: {
-        console.log('Чай');
-        break;
-    }
-    case 5: {
-        console.log('Лимонад');
-        break;
-    }
-    default: {
-        console.log('Оберіть щось із меню');
-    }
-}
-
-
 /* Задача 2 (за бажанням)
 
 Зробити калькулятор =)
@@ -58,3 +14,53 @@ P.S. Повна творча свобода.
 Якщо ви бачите рішення цієї задачі якось по-іншому - Ви можете реалізувати своє бачення калькулятора.
 
 */
+
+const operand1 = Number(prompt('Введіть перше число'));
+const operand2 = Number(prompt('Введіть друге число'));
+const action = prompt('Введіть потрібну дію: +, -, *, /');
+
+function calculator(operand1, operand2, action) {
+    let result; // тут буде лежати остаточний результат, після дій над операндами
+
+    switch(action) {
+        case '+': {
+            result = sum(operand1, operand2);
+            break;
+        }
+        case '-': {
+            result = substract(operand1, operand2);
+            break;
+        }
+        case '*': {
+            result = multy(operand1, operand2);
+            break;
+        }
+        case '/': {
+            result = divide(operand1, operand2);
+            break;
+        }
+        default: {
+            result = 'Сталася помилка! Введіть правильну дію +, -, *, /';
+        }
+    }
+
+    console.log(result);
+}
+
+calculator(operand1, operand2, action);
+
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multy(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
