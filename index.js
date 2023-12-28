4d77e30b66c50a7111638615a26229ff9efe1eae
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-28 18:07:03 +0200

    Menu task resolve

--- index.js
+++ index.js
@@ -1,66 +1,3 @@
-// Задача: Написати программу, яка перевіряє число на те, чи ділиться воно на 5, 3, 2
-
-// const number = Number(prompt('Введіть число: '));
-
-// if(number % 5 === 0) {
-//     console.log(number + ' ділиться на 5');
-// } else if(number % 3 === 0) {
-//     console.log(number + ' ділиться на 3');
-// } else if(number % 2 === 0) {
-//     console.log(number + ' ділиться на 2');
-// } else {
-//     console.log(number + ' націло не ділить ні на 5, ні на 3, ні на 2');
-// }
-
-// switch - спосіб реалізації вибору з декількох можливих варіантів в залежності від значення виразу
-// Конструкція switch використовується у випадках коли перевіряється одна змінна на різні значення
-
-/* Синтаксис
-
-switch(вираз - якась змінна) {
-    case значення_виразу: робимо щось одне;
-    case значення2_виразу: робимо щось інше;
-    case значення3_виразу: робимо ще щось інше;
-    default: робимо щось, якщо інші значення не підійшли
-}
-
-*/
-
-// Задача: нам приходить номер місяця. За номером місяця потрібно визначити пору року
-
-const monthNumber = 12;
-
-switch(monthNumber) {
-    case 1:
-    case 2: 
-    case 12: {
-        console.log('зима');
-        break;
-    }
-    case 3: 
-    case 4:
-    case 5: {
-        console.log('весна');
-        break;
-    }
-    case 6: 
-    case 7: 
-    case 8: {
-        console.log('літо');
-        break;
-    }
-    case 9: 
-    case 10: 
-    case 11: {
-        console.log('осінь');
-        break;
-    }
-    default: {
-        console.log('Введи число від 1 до 12');
-        break;
-    }
-}
-
 /* Задача 1 :^
 
 prompt
@@ -75,6 +12,34 @@ prompt
 Зробіть, будь ласка, через switch-case
 
 */
+
+const userChoice = Number(prompt('Оберіть напій: \n1 - Сік \n2 - Вода \n3 - Кава \n4 - Чай \n5 - Лимонад'));
+
+switch(userChoice) {
+    case 1: {
+        console.log('Сік');
+        break;
+    }
+    case 2: {
+        console.log('Вода');
+        break;
+    }
+    case 3: {
+        console.log('Кава');
+        break;
+    }
+    case 4: {
+        console.log('Чай');
+        break;
+    }
+    case 5: {
+        console.log('Лимонад');
+        break;
+    }
+    default: {
+        console.log('Оберіть щось із меню');
+    }
+}
 
 
 /* Задача 2 (за бажанням)
