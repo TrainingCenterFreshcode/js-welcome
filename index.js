6292a5e919b8b30e2b2a3926e56c5600084a53c8
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-29 19:20:44 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-/* Loops - Цикли
 
-while (умова) {
-  код, який повторюється, поки умова = true
-}
+// variant 1
 
-Написати у консолі 10 разів цифру 1
+const correctPassword = 'qwerty1234';
 
+// let userPass = prompt('Введіть свій пароль');
 
-while(кількість_консоль_логів < 10) {
-  console.log(1);
-  кількість_консоль_логів++;
-}
-
-*/
-
-// let iterator = 0; // лічильник консоль.логів
-
-// Ітерація - один прохід циклу (коло)
-
-// while(iterator < 10) {
-//   console.log(iterator);
-//   iterator++;
+// while(userPass !== correctPassword) {
+//   userPass = prompt('Помилка! Введіть пароль ще раз.');
 // }
 
-// Нескінченний цикл
+// variant 2
 
 // while(true) {
-//   console.log('Не вірно');
-//   break;
+//   const userPass = prompt('Введіть свій пароль');
+//   if(userPass === correctPassword) {
+//     break;
+//   }
 // }
 
-// Задача: зробити 10 повторів (ітерацій) циклу і вивести на консоль парні значення лічильника
+// variant 3
 
-let i = 0; // i += 2 || 0 -> 2 -> 4 -> 6 -> 8 -> 10 (false)
-          //  i++    || 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 (false)
-while(i < 10) {
-  if(i % 2 === 0) {
-    console.log(i);
-  }
+// let isPasswordCorrect = false;
+// while(!isPasswordCorrect) { // isPasswordCorrect === false
+//   const userPass = prompt('Введіть свій пароль');
 
-  i += 2;
-}
-
-/*
-
-Дано правильний пароль = qwerty1234
-
-prompt
-Запитуйте у користувача пароль через prompt, поки пароль, який вводить користувач, не співпаде з тим еталонним паролем, який лежить у змінній
-
-*/
+//   if(userPass === correctPassword) {
+//     isPasswordCorrect = true;
+//   }
+// }
