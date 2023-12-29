95fa337f2a1213fbe2e7372816c4970dc8bbc81e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2023-12-29 19:46:03 +0200

    do-while

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
+// Loops - Цикли
 
-// variant 1
+/*
+while - цикл з передумовою
+
+while (умова) {
+  тут буде блок коду, який буде циклічно виконуватись
+}
+
+do-while - цикл з післяумовою
+
+do {
+  тут буде блок коду, який буде циклічно виконуватись
+} while (умова)
+
+*/
 
 const correctPassword = 'qwerty1234';
+let userPass; // пароль з промпту
 
-// let userPass = prompt('Введіть свій пароль');
-
-// while(userPass !== correctPassword) {
-//   userPass = prompt('Помилка! Введіть пароль ще раз.');
-// }
-
-// variant 2
-
-// while(true) {
-//   const userPass = prompt('Введіть свій пароль');
-//   if(userPass === correctPassword) {
-//     break;
-//   }
-// }
-
-// variant 3
-
-// let isPasswordCorrect = false;
-// while(!isPasswordCorrect) { // isPasswordCorrect === false
-//   const userPass = prompt('Введіть свій пароль');
-
-//   if(userPass === correctPassword) {
-//     isPasswordCorrect = true;
-//   }
-// }
+do {
+  userPass = prompt('Введіть свій пароль');
+} while(userPass !== correctPassword)
+// до тих пір поки
