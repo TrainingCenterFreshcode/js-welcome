bdd6283ba7740a4a64d302e1c6b0074e307cc300
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-05 19:56:22 +0200

    Practise task 1 resolve

--- index.js
+++ index.js
@@ -1,79 +1,41 @@
-/* Розробка програми для керування книжковим магазином
+/*
 
-Предметна область: В книжковому магазині у нас буде багато книг. Ми їх продаємо
+Задача 1
 
-Всі наші книги по суті можна представити у вигляді об'єкта з однаковою.
-
-done -->> Задача: створити конструктор об'єктів для представлення книг з наступними властивостями.
-Назва книги, автор, рік видання, ціна.
-Спректувати методи для отримання і встановлення значень цих властивостей.
-
-done -->> Задача 2: Зробити для всіх книг метод, який би повернув ціну книги, але зі знижкою
+Зробити функцію-конструктор Country, яка приймає в якості аргументів:
+- назва країни
+- популяція (population)
+- площа (area)
+Спроектувати метод густоти населення
+P.S. Густота наеселення = популяція цієї країни / площу цієї ж країни
 
 */
 
-function Book(title, author, year, price) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-  this.price = price;
+function Country(name, population, area) {
+  this.name = name;
+  this.population = population;
+  this.area = area;
 
-  this.getTitle = function() {
-    // реалізувати, наприклад, перевірку права на доступ до властивості
-    return this.title;
-  }
-
-  this.getAuthor = function() {
-    // реалізувати, наприклад, перевірку права на доступ до властивості
-    return this.author;
-  }
-
-  this.getYear = function() {
-    // реалізувати, наприклад, перевірку права на доступ до властивості
-    return this.year;
-  }
-
-  this.getPrice = function() {
-    // реалізувати, наприклад, перевірку права на доступ до властивості
-    return this.price;
-  }
-
-  this.setTitle = function(newTitle) {
-    // реалізувати, наприклад, якісь перевірки
-    this.title = newTitle;
-  }
-
-  this.setAuthor = function(newAuthor) {
-    // реалізувати, наприклад, якісь перевірки
-    this.author = newAuthor;
-  }
-
-  this.setYear = function(newYear) {
-    // реалізувати, наприклад, якісь перевірки
-    this.year = newYear;
-  }
-
-  this.setPrice = function(newPrice) {
-    // реалізувати, наприклад, якісь перевірки
-    this.price = newPrice;
-  }
-
-  this.calculateDiscountedPrice = function(discountPercentage) {
-    const discountedPrice = this.price - (this.price * (discountPercentage / 100));
-    return discountedPrice;
+  this.getDensity = function() {
+    return this.population / this.area;
   }
 }
 
-const book1 = new Book('Чотири вітри', 'Крістін Генна', 2020, 450);
+const UK = new Country('United Kingdom', 67333000, 243610);
 
-// console.log(book1.getTitle());
-// console.log(book1.getAuthor());
-// console.log(book1.getYear());
-// console.log(book1.getPrice());
+/*
 
-// book1.setTitle('New Title');
-// book1.setAuthor('New Author');
-// book1.setYear(2025);
-// book1.setPrice(350);
+Задача 2
 
-console.log(book1.calculateDiscountedPrice(10));
+Зробити функцію-конструктор Авто, яке має:
+- назву
+- максимальну швидкість
+- поточну швидкість
+Спроектувати методи:
+- метод прискорення (accelearte) - приймає в якості аргумента певне прискорення
+Зауважте, що при прискоренні, поточна швидкісь не може бути більшою за максимальну швидкість
+- метод сповільнення (deaccelerate) - приймає в якості аргумента певне сповільнення
+Зауважте, що при сповільненні, поточна швидкість не може бути менше за 0
+- метод зупинки (stop)
+
+*/
