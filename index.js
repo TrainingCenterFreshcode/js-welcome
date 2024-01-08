0820f8ca180837380d6f21b1f0c1ab20788288c1
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-08 18:56:30 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ const valueFromUser = 19;
                     // 19
 console.log(greets[valueFromUser]); // Hello
 // greets[19]
+
+
+
+/*
+
+Задача: у нас є об'єкт, ключем в якому є день тижня, а значенням - плани на цей день
+Прийняти від користувача (через промпт) назву дня тижня і видати алерт, що заплановано у користувача на цей день
+
+*/
+
+const obj = {
+  Mon: 'drive lesson',
+  Tue: 'dantist visit',
+  Wed: 'go to party',
+  Thu: 'hard work',
+  Fri: 'chill',
+  Sat: 'go to park'
+}
+
+const userValue = prompt('Type your week day to get your plan: \n Monday = Mon, \n Tuesday = Tue, \n ...');
+alert(obj[userValue] ? obj[userValue] : 'Error happened');
