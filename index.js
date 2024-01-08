1113b1bddeb37156de962bfaa8b09a874d502cd7
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-08 19:32:53 +0200

    Practise task 1 resolve

--- index.js
+++ index.js
@@ -25,3 +25,22 @@ object - об'єкт, властивості (ключі) якого ми хоч
 for (let key in user) {
   console.log(`${key} ---> ${user[key]}`); // key ---> value
 }
+
+/*
+
+Задача: написати функцію, яка приймає об'єкт і виводить всі значення всіх властивостей, оформлені *...*
+А ключі - "..."
+
+*/
+
+const obj = {
+  key1: 'value 1'
+}
+
+// "key1": *value1*
+
+function stylingObject(obj) {
+  for(let key in obj) {
+    console.log(`"${key}": *${obj[key]}*`);
+  }
+}
