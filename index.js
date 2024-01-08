9c5936c2b15a91e83ea7e86925a8a86c86ae5c99
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-08 18:40:32 +0200

    Another example of how computing properties work

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const user = {
-  'name': 'John', // Ключ - 'name', значення - 'John'
-  'favorite color': 'red',
-  0: 'value'
+/*
+
+Задача: у нас є об'єкт з різними версіями привітань для користувача.
+Вивести користувачу привітання, залежно від значення, яке він ввів.
+
+*/
+
+const greets = {
+  15: 'Hi',
+  17: 'Ya, whats up',
+  19: 'Hello',
+  30: 'Good morning!'
 }
 
-// Ключем в об'єкті може бути рядок або тип даних Symbol
-
-// . - оператор доступу за ім'ям властивості (коли мова йде про валідні ідентифікатори)
-
-console.log(user.name); // John
-// user.'favorite color'; -->> syntax error
-// user.0; -->> syntax error
-
-// [] - оператор доступу до обчислювальної властивості (коли мова йде про невалідні ідентифікатори)
-console.log(user['favorite color']); // red
-
-console.log(user[0]); // value
-
-
-const someObj = {
-  2: 10
-}
-
-// Задача: отримати значення, яке знаходиться під ключем 2
-
-console.log(someObj[2]);
-console.log(someObj['2']);
+const valueFromUser = 19;
+                    // 19
+console.log(greets[valueFromUser]); // Hello
+// greets[19]
