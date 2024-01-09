1bbb1fa7228d04bf6598de2e00769a68c2057c86
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-09 18:50:38 +0200

    Array welcome

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
-/*
+// У нас є якийсь юзер
 
-Задача: у нас є об'єкт з зарплатами по всіх відділах.
-Написати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів
+const userTelephone1 = '380994875717';
+const userTelephone2 = '380994875358';
+const userTelephone3 = '380994871234';
 
-*/
-
-const departmentSalaryInCompany1 = {
-  HR: 120000,
-  development: 5500000,
-  PR: 50000,
-  marketing: 120000,
-  assistant: undefined
+const userTelephones = {
+  0: '380994875717',
+  1: '380994875358',
+  2: '380994871234'
 }
 
-const departmentSalaryInCompany2 = {
-  HR: 500000,
-  JS: 5500000,
-  PR: 50000,
-  QA: 800000,
-  clojure: 250000,
-  frontend: 365800,
-  strategyExecution: undefined,
-  cLevelel: undefined
-}
+// Масив (Array)
+
+// ОГолошення масиву
+
+// 1 спосіб
+const arr = [2, 4, 6, 7, 150, 789, 123];
+
+// 2 спосіб
+const arr2 = new Array(2, 4, 6, 7);
+
+// Cпособи доступу до елементів масиву
+arr[0]; // зчитування значень елементів масиву
+arr[0] = 200; // присвоєння нових значень для елементів масиву
+
+// +Задача: вивести всі значення масиву в консолі
 
 
-function sumSalary(salaryObject) {
-  let sum = 0;
+// варіант 1
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
 
-  for(let key in salaryObject) {
-    if(typeof salaryObject[key] === 'number') {
-      sum += salaryObject[key];
-    }
+// варіант 2
+
+// for(let i = 0; i < arr.length; i++) {
+//   // 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+//   console.log(arr[i]);
+// }
+
+
+// Задача: у нас є масив чисел. Вивести на консоль тільки парні числа цього масиву
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i = 0; i < numbersArray.length; i++) {
+  if(numbersArray[i] % 2 === 0) {
+    console.log(numbersArray[i]);
   }
-
-  return sum;
 }
