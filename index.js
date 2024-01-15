a9ea5fc00009a815e71c4043eb3570e50dfa92d2
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-15 20:47:33 +0200

    Add pop method + task

--- index.js
+++ index.js
@@ -20,10 +20,30 @@ function MyArray() {
   this.length = 0;
 
   this.push = function (value) {
-    // значення додати у КІНЕЦЬ масиву
-    this[this.length] = value;
-    this.length++;
+    for(let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
     return this.length;
+  }
+
+  this.pop = function() {
+    if(this.length > 0) {
+      // 1. Зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // 2. Видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // 3. Зменшити довжину масиву на 1
+      this.length--;
+      // 4. Повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
+
+  this.forEach = function(callback) {
+    // ?
   }
 }
 
@@ -32,4 +52,4 @@ arr.push(1);
 arr.push(2);
 
 // Перепишіть push таким чином, щоб push міг приймати та додавав до масиву будь-яку кількість переданих елементів
-// arr.push(3, 2, 5, 10, 12, 33);
+arr.push(3, 2, 5, 10, 12, 33);
