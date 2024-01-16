31aeac0c882a661e0fe1b83b12b978f9fff3b6ad
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-16 19:01:50 +0200

    __proto__ literal

--- index.js
+++ index.js
@@ -59,3 +59,30 @@ arr.push(3, 2, 5);
 arr.forEach((item) => {
   console.log(item ** 2);
 });
+
+
+const cat = {
+  name: 'Barsik',
+  color: 'red',
+  age: 1
+}
+
+const cat2 = {
+  name: 'Murzik',
+  color: 'black',
+  age: 5
+}
+
+const catMethods = { // прототип
+  run: function() {
+    console.log('Cat is running');
+  },
+  meow: function() {
+    console.log('Meow!');
+  }
+}
+
+// Як прикрутити catMethods до об'єкту cat?
+
+cat.__proto__ = catMethods; // прототипне посилання
+cat2.__proto__ = catMethods;
