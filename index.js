4b87875f94c03002c92cc71b0394e712b28ca8ae
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-18 20:54:24 +0200

    Double Loop

--- index.js
+++ index.js
@@ -1,64 +1,41 @@
-'use strict';
+// Вкладені цикли (Double Loop)
 
-// rest operator - оператор залишкових параметрів
-// rest - залишок (з англ.)
-// .., .., .., .., -->> []
+/*
+Вивести на консоль таблицю множення від 1 до 5
 
-const arrowSum = (...restArrayOfNumbers) => {
-  let sum = 0;
+1x1 = 1
+1x2 = 2
+....1x5 = 5
 
-  for(let i = 0; i < restArrayOfNumbers.length; i++) {
-    sum += restArrayOfNumbers[i];
+2x1 = 2
+2x2 = 4
+
+....
+
+5x1 = 5
+5x2 = 10
+5x3
+5x4
+5x5
+
+
+*/
+
+// for(let i = 1; i <= 5; i++) { // 1 - число, для якого ми виводимо таблицю множення
+//   for(let j=1; j <= 5; j++) { // 1*1, 1*2, 1*3, 1*4, 1*5 - числа, на які ми множимо число з зовнішнього циклу
+//     console.log(`${i} x ${j} = ${i*j}`);
+//   }
+// }
+
+const numbers = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+
+// Задача: знайти суму елементів масиву numbers
+
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) { // зовнішній цикл контролює масив, який ми переглдаємо
+  for(let j = 0; j < numbers[i].length; j++) { // внутрішній цикл сумує всі елементи поточного просматріваемого вкладеного масиву
+    sum += numbers[i][j]; // отримуємо доступ до поточного елементу просматріваемого вкладеного масиву
   }
-
-  return sum;
 }
 
-// spread operator
-// spread - розпакувати (з англ.)
-// [] -->> .., .., .., ..,
-
-const numbers = [1, 2, 3, 4, 5];
-
-function sum (a, b, ...restArray) {
-  console.log(restArray);
-  return a + b;
-}
-
-// console.log(sum(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
-console.log(sum(...numbers)); // spread operator
-// розпакували всі значення масиву numbers в виклик функції sum
-
-// Як розрізняти rest і spread оператор (...)?
-
-// Якщо оператор стоїть у аргументах функції, то це rest - він просто бере і збирає всі залишки параметрів
-// Якщо ми використовуємо оператор на масиві, то це spread - він бере і розбиває масив на купу елементів
-
-
-
-
-
-// Приклад використання spread оператора № 1
-
-const numbersMath = [2, 3, 1, 5, 4];
-
-Math.min(...numbersMath); // 1
-
-// Приклад використання spread оператора № 2
-
-// Задача: зробити масив з назвою copyArray в який будуть входити всі елементи з масиву numbersMath
-
-const copyArray = [...numbersMath];
-
-
-// Задача
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [10, 11, 12, 13, 14, 15];
-
-// Потрібно зклеїти ці 2 масиви
-// Не можна використовувати метод concat
-// Використовуйте spread оператор
-// [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
-
-const resultArray = [...array1, ...array2];
+console.log(sum);
