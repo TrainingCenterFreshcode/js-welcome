058a5bf6cce9f9a9d5966b9826105ab3b2cf438e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-18 19:26:24 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -23,12 +23,30 @@ console.log(sum(1, 2, 3, 5, 10, 25)); // 3
 
 // Задача: написати стрілочну функцію, яка сумує будь-яку кількість чисел
 
-const arrowSum = (...restArrayOfNumbers) => {
-  let sum = 0;
+// const arrowSum = (...restArrayOfNumbers) => {
+//   let sum = 0;
 
-  for(let i = 0; i < restArrayOfNumbers.length; i++) {
-    sum += restArrayOfNumbers[i];
-  }
+//   for(let i = 0; i < restArrayOfNumbers.length; i++) {
+//     sum += restArrayOfNumbers[i];
+//   }
+
+//   return sum;
+// }
+
+/* Задача
+
+Напишіть варіант функції arrowSum з використанням метода reduce
+При виклику reduce передайте йому стрілковий колбек =)
+
+*/
+
+const arrowSum = (...restArray) => { // variant 1
+  const sum = restArray.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 
   return sum;
 }
+
+// variant 2
+// const arrowSum = (...restArray) => restArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
