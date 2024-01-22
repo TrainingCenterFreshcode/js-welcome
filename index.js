6ca7f8d615e0f7117a2ba4a429c732bf48aa7a10
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-22 18:31:13 +0200

    Practise task 2 resolve

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
 
-/* Задача 1
-
-Написати функцію checkSpam, яка повертає true, якщо переданий рядок містить слова 'xxx' або 'viagra'
-Якщо заборонених слів у рядку немає - повертається false
-
-checkSpam('buy ViAgRa now'); // true
-checkSpam('free xxxxxxx'); // true
-checkSpam('innocent rabbit'); // false
-
-*/
-
-// function checkSpam(str) { // variant 1
-//   const lowStr = str.toLowerCase();
-//   return lowStr.includes('viagra') || lowStr.includes('xxx');
-// }
-
-function checkSpam(str) { // variant 2
-  const spamArray = ['viagra', 'xxx', 'drugs'];
-
-  for(let i = 0; i < spamArray.length; i++) {
-    if(str.toLowerCase().includes(spamArray[i])) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-console.log(checkSpam('buy ViAgRa now'));
-console.log(checkSpam('free xxxxxxx'));
-console.log(checkSpam('innocent rabbit'));
-
 /* Задача 2
 
 Написати функцію, яка перевіряє, чи є переданий їй рядок - паліндромом. не зважаючи на регістр
@@ -42,3 +10,24 @@ Namman - паліндром
 
 */
 
+
+function checkPalindrom(str) {
+  /*
+    +1. Приймаємо строку
+    +2. Приводимо строку до нижнього регістру
+    +3. Перевернути строку
+    +4. Порівнюємо перевернуту строку з пункту 3 з оригінальною строкою
+  */
+
+  // Реалізація 1 і 2 пункту
+  const originalStr = str.toLowerCase();
+
+  // Реалізація 3 пункту
+  const reversedStr = originalStr.split('').reverse().join('');
+
+  // Реалізація 4 пункту
+  return originalStr === reversedStr;
+}
+
+console.log(checkPalindrom('Papa')); // false
+console.log(checkPalindrom('Anna')); // true
