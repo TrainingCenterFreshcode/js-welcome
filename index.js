3fd6c847c3010018de524392888f527959228c53
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-23 19:23:58 +0200

    Errors and try..catch

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+// Задача: написати функцію суми додатніх чисел
 
+/**
+ * Функція знаходить суму двох додатніх чисел
+ * @param {number} a перший доданок
+ * @param {number} b другий доданок
+ * @returns {number} сума
+ * @throws {RangeError} якщо а або b - не є цілим числом
+ * @throws {TypeError} якщо а або b - не є числом
+ */
+function sumOfPositiveNumber(a, b) {
+  if(a < 0 || b < 0) {
+    // маємо "викинути" помилку
+    const error = new RangeError('Якесь з чисел менше 0');
+    throw error; // return, тільки для помилок
+  }
 
-/** 
- * Повертає x у ступені n
- * 
- * @param {number} x число, яке треба піднести до степеня
- * @param {number} n степінь, у яку потрібно піднести
- * @return {number} x піднесене у n-нну степінь
-*/
-function pow(x, n) {
-  return x ** n;
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Параметри a та b мають бути числами!');
+  }
+
+  return a + b;
 }
+
+// throw <об'єкт_помилки>
+// throw - кинути
+
+try {
+  // прописуєте якийсь код, очікуючи, що тут може виникнути помилка
+  sumOfPositiveNumber(1, -2);
+} catch(error) {
+  console.log(error.message);
+}
+
+console.log('Ususal code flow');
