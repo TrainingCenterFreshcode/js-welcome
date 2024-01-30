943256de82ebf739ddddfabcd808e3d27c820119
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-01-30 20:47:33 +0200

    Add map method to MyArray

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-/*
+class MyArray {
+  constructor() {
+    this.length = 0;
+  }
 
-Symbol - представляє собою унікальний ідентифікатор.
-Кожен створений символ має свій власний унікальний ідентифікатор, який ніде і ніколи не буде повторюватись
+  push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }
 
-Особливість символів - вони завжди унікальні
-Кожен створений символ, матиме унікальний ідентифікатор, який ніде і ніколи не буде повторюватись
+  pop() {
+    if (this.length > 0) {
+      // 1. Зберегти останній елемент
+      const lastItem = this[this.length - 1];
+      // 2. Видалити останній елемент з масиву
+      delete this[this.length - 1];
+      // 3. Зменшити довжину масиву на 1
+      this.length--;
+      // 4. Повернути останній елемент
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  }
 
-Гарантія унікальності - єдине, для чого Symbol і існує
+  forEach(callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  }
 
-*/
+  map(callback) {
+    // 1. Просто створили пустий масив - він буде нашим результуючим масивом
+    const resultArray = new MyArray();
 
-const mySymbol = Symbol(); // без new!!
+    // 2. Пройтись по поточному масиву від початку і до кінця
+    for(let i = 0; i < this.length; i++) {
+      // Запихуємо у результуючий масив значення, яке нам буде повертати (return) колбек
+      resultArray.push(callback(this[i], i, this));
+    }
 
-// const symb2 = Symbol('My second symbol'); // label for human
-
-const obj = {
-  // У об'єктах ключами можуть бути рядки або символи
-  'test': 1,
-  [mySymbol]: 123
+    // 3. Повернути як результат роботи методу map - результуючий масив
+    return resultArray;
+  }
 }
 
-console.log(obj[mySymbol]); // 123
+const arr = new MyArray();
+
+arr.push(3, 2, 1, 2, 3);
+
+// Задача: створити новий масив, який буде містити квадрати чисел з масиву arr
+
+const doubleNumbers = arr.map((item) => {
+  return item ** 2;
+});
+
+// const doubleNumbers = arr.map(item => item ** 2);
