8a9b4862c4dfc62164de2778b94d2188dfa9d489
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-01 20:23:35 +0200

    Binary search and algorithms

--- index.js
+++ index.js
@@ -1,60 +1,72 @@
-class MyArray {
-  constructor() {
-    this.length = 0;
-  }
+const arr = [2, 4, 1, 3, 7, 3, 2, 1, 3, 5, 2, 8, 2, 4, 1];
 
-  push() {
-    for (let i = 0; i < arguments.length; i++) {
-      this[this.length] = arguments[i];
-      this.length++;
-    }
-    return this.length;
-  }
+// Задача: реалізувати функцію лінійного пошуку якогось значення в масиві
 
-  pop() {
-    if (this.length > 0) {
-      // 1. Зберегти останній елемент
-      const lastItem = this[this.length - 1];
-      // 2. Видалити останній елемент з масиву
-      delete this[this.length - 1];
-      // 3. Зменшити довжину масиву на 1
-      this.length--;
-      // 4. Повернути останній елемент
-      return lastItem;
-    } else {
-      return undefined;
+function linearSearch(array, value) { // лінійна складність алгоритму
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === value) {
+      return i;
     }
   }
 
-  forEach(callback) {
-    for (let i = 0; i < this.length; i++) {
-      callback(this[i], i, this);
-    }
-  }
-
-  map(callback) {
-    // 1. Просто створили пустий масив - він буде нашим результуючим масивом
-    const resultArray = new MyArray();
-
-    // 2. Пройтись по поточному масиву від початку і до кінця
-    for(let i = 0; i < this.length; i++) {
-      // Запихуємо у результуючий масив значення, яке нам буде повертати (return) колбек
-      resultArray.push(callback(this[i], i, this));
-    }
-
-    // 3. Повернути як результат роботи методу map - результуючий масив
-    return resultArray;
-  }
+  return -1;
 }
 
-const arr = new MyArray();
+// console.log(linearSearch(arr, 2));
 
-arr.push(3, 2, 1, 2, 3);
+// Лінійна складність, коли Big O, тобто, коли стільки ітерацій (операцій), скільки в нас вхідних даних
 
-// Задача: створити новий масив, який буде містити квадрати чисел з масиву arr
 
-const doubleNumbers = arr.map((item) => {
-  return item ** 2;
-});
 
-// const doubleNumbers = arr.map(item => item ** 2);
+
+
+// Квадратична складність
+
+// Задача: вивести таблицю множення
+
+function multyTable(limit) { // квадратична складність
+  const table = [];
+  for(let i = 1; i <= limit; i++) {
+    for(let j = 1; j <= limit; j++) {
+      table.push(`${i} * ${j} = ${i * j}`)
+    }
+  }
+
+  return table;
+}
+
+// multyTable(1); // 1 (1^2 = 1)
+// multyTable(2); // 2 (2^2 = 4)
+// multyTable(3); // 3 (3^2 = 9)
+
+// Квадратична складність - коли ми на виході отримуємо кількість вхідних даних (n) в квадраті
+
+
+
+
+// Логарифмічна складність ---> 100 вхідних даних припадає приблизно 6 операцій
+
+const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function binarySearch(array, whatToFind) { // need review!!
+  if(whatToFind > array[array.length - 1]) {
+    return -Infinity; // на випадок, якщо такого числа точно немає у масиві
+  }
+
+  let start = 0;
+  let end = array.length - 1;
+  let middle = Math.round((start + end) / 2);
+
+  while(true) {
+    if(array[middle] === whatToFind) {
+      return middle;
+    }
+    if(array[middle] < whatToFind) {
+      start = middle;
+      middle = Math.ceil((start + end) / 2);
+    } else {
+      end = middle;
+      middle = Math.ceil((start + end) / 2);
+    }
+  }
+}
