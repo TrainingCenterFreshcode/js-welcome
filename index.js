4cb7c77826be30afa50c30e62956bdd517db5aba
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-05 18:24:14 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 
-// Задача: зберегти розклад занять, використовуючи дні тижня як ключі, а значеннями будуть списки занять, які відбуваються у відповідний день
 
-const schedule = new Map();
+const user1 = {
+  firstName: 'Alex',
+  lastName: 'Doe',
+  id: 1
+}
 
-// Додавання занять до розкладу
-schedule.set('Понеділок', ['Математика', 'Фізика']);
-schedule.set('Вівторок', ['Англійська мова', 'Всесвітня історія']);
-schedule.set('Середа', ['Хімія', 'Географія']);
-schedule.set('Четвер', ['Зарубіжна література', 'Біологія']);
-schedule.set('П\`ятниця', ['Інформатика', 'Фізкультура']);
+const user2 = {
+  firstName: 'John',
+  lastName: 'Doe',
+  id: 2
+}
 
-// Отримання розкладу занять для певного дня
-console.log(schedule.get('Понеділок'));
+const johnMessages = ['hello', 'How are you?'];
+const alexMessages = ['hi!', 'I`m fine'];
 
-// Перевірка, чи є розклад для певного дня
-console.log(schedule.has('Субота'));
+const messageMap = new Map();
+messageMap.set(`${user1.firstName} ${user1.lastName}`, alexMessages);
+messageMap.set(`${user2.firstName} ${user2.lastName}`, johnMessages);
 
-// Отримання кількості днів занять в розкладі
-console.log(schedule.size);
+/*
+
++ Задача: за допомогою Map зв'язати користувача з його повідомленнями, щоб по ID користувача, можна було знайти його повідомлення
+
+P.S. Ви можете підглядати у коміт з вирішенням задачі про розклад занять:
+https://github.com/TrainingCenterFreshcode/js-welcome/blob/244c766334f1a66215b46893896135bdbf501f72/index.js
+
+*/
