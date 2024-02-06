aa5f871cc2aa8fdb23ee807ae2f8fbf7636f768b
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-06 19:02:28 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -18,24 +18,28 @@ const monitor = {
     resolution: '4K'
 }
 
-// const height = monitor.sizes.height.value;
 
-// const {resolution, color: monitorColor} = monitor;
 
-// console.log(resolution);
-// console.log(monitorColor);
 
-// Задача: витягнути value для висоти та ширини монітору
+// У нас є об'єкт юзера
 
-// const {sizes: {height: {value: heightValue}, width: {value: widthValue}}, brightness, refresh: {value: refreshValue}} = monitor;
+const user = {
+    name: 'John',
+    age: 30,
+    address: {
+        city: 'Kyiv',
+        country: 'Ukraine'
+    },
+    contacts: {
+        email: 'john@gmail.com',
+        phone: '+380994875717'
+    }
+}
 
-// console.log(heightValue);
-// console.log(widthValue);
-// console.log(refreshValue);
+// Задача: з використанням деструктуризації отримати значення name, city, email та phone
 
-const {sizes: {height: {scale: heightScale}, width: {scale: widthScale}}} = monitor;
-
-console.log(heightScale);
-console.log(widthScale);
-
-const {color, brightness, resolution, ...restOfMonitor} = monitor;
+const {name, address: {city}, contacts: {email, phone}} = user;
+console.log(name);
+console.log(city);
+console.log(email);
+console.log(phone);
