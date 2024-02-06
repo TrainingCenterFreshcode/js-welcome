d85fd326752f570b594824e310ad5a88d8bdd81a
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-06 20:53:39 +0200

    Closure

--- index.js
+++ index.js
@@ -1,116 +1,57 @@
-const monitor = {
-    sizes: {
-        height: {
-            value: 30,
-            scale: 'cm // 1'
+// Замикання дозволяє функціям зберігати доступ до змінних, навіть коли ці функції завершують своє виконання
+// Замикання - здатність функції запам'ятовувати локальну область видимості
+
+let value = 10; // глобальна область видимості
+
+
+function wrapper() {
+    let value = 20; // локальна область видимості
+
+    console.log('WRAPPER function', value);
+
+    return function log() {
+        console.log('LOG function', value);
+    }
+}
+
+
+
+
+
+
+
+//////////////
+
+
+// function counter() {
+//     let i = 0;
+//     i++;
+//     return i;
+// }
+
+function makeCounter() {
+    let i = 0;
+    return function() {
+        return i++; // i - змінна у замиканні
+    }
+}
+
+
+//////
+
+
+function makeCounterVersion2() {
+    // Якщо буде бажання - спробуйте зробити методи, які будуть збільшувати/зменшувати counter на певну кількість одиниць
+    let counter = 0;
+
+    return {
+        increment() {
+            return ++counter;
         },
-        width: {
-            value: 50,
-            scale: 'cm // 2'
+        decrement() {
+            return --counter;
         }
-    },
-    brightness: 750,
-    refresh: {
-        value: 144,
-        scale: 'Ggrc'
-    },
-    color: 'black',
-    resolution: '4K'
+    }
 }
 
-
-
-
-/*
-
-Існує 3 види деструктуризації у JS:
-+ 1. Деструктуризація об'єктів
-+ 2. Деструктуризація вхідних параметрів
-+ 3. Деструктуризація масивів
-
-*/
-
-
-
-function getFullName({firstName, lastName, ...restObject}) { // все інше окрім firstName, lastName ігнорується
-    console.log(restObject);
-    return `${firstName} ${lastName}`;
-}
-
-const user2 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 42,
-    geolocation: '42.2213123 56.11214545',
-    browser: 'Chrome'
-}
-
-console.log(getFullName(user2));
-
-
-
-
-
-
-
-
-
-
-const arr = [1, 2, 3, 4, 5, 6];
-// const firstElement = arr[0];
-
-const [firstElement, secondElement, ...restOfArr] = arr;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const user = {
-    name: 'John',
-    age: 30,
-    address: {
-        city: 'Kyiv',
-        country: 'Ukraine'
-    },
-    contacts: [
-        {email: 'john@gmail.com'},
-        {phones: ['+380994875717', '+380994875236', '+38096589631']}
-    ]
-}
-
-// const {contacts: {phones}} = user;
-
-// const [firstJohnNumber, secondJohnNumber, thirdJohnNumber] = phones;
-
-// const {contacts: {phones: [firstJohnNumber, secondJohnNumber, thirdJohnNumber]}} = user;
-
-
-
-
-
-const {contacts: [emailObject, phonesArray]} = user;
-
-const { email } = emailObject;
-const {phones: [firstJohnNumber, secondJohnNumber, thirdJohnNumber]} = phonesArray;
-
-// console.log(email);
-// console.log(firstJohnNumber);
-// console.log(secondJohnNumber);
-// console.log(thirdJohnNumber);
+const fnObj = makeCounterVersion2();
